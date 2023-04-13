a83d90327631d751ab1a86118ec5cda9ba0e429d
Jayde Mitchell <jayde@jaydemitchell.com>
2023-04-13 10:55:31 -0700

    fixed fallback

--- src/LoaderFallback.tsx
+++ src/LoaderFallback.tsx
@@ -2,7 +2,7 @@ function LoaderFallback(): JSX.Element {
   return (
     <div>
       <h1 style={{ margin: 0 }}>JAYDE MITCHELL</h1>
-      <div style={{ textAlign: 'right' }}>01</div>
+      <div style={{ textAlign: 'right' }}>0</div>
     </div>
   );
 }
